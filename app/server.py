5d0f984e44a9a223521f8fc6045bd5204b38a07d
leosheck <32866405+leosheck@users.noreply.github.com>
2019-05-07 20:59:29 +1200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,12 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://www.dropbox.com/s/f8hytgsqlq1o17i/bird_classifier?dl=1'
+export_file_name = 'bird_classifier.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ["Australian magpie", "Bellbird", "Black-backed gull", "Blackbird", "Chaffinch", "Dunnock", "Fantail", 
+          "Goldfinch", "Greenfinch", "Grey warbler", "House sparrow", "Kereru", "Myna", "Red-billed gull", 
+          "Redpoll", "Rock pigeon", "Silvereye", "Song Thrush", "Starling", "Tui", "Welcome Swallow", "Yellowhammer"]
 path = Path(__file__).parent
 
 app = Starlette()
